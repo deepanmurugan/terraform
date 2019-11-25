aee34ef0da5bcff4e740e0c55b54b3bdb584bcda
Deepan M <55130173+deepanmurugan@users.noreply.github.com>
2019-11-25 15:37:51 +0530

    Delete variable.tf

--- variable.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_cidr_block" { default = "10.1.0.0/16" }
-variable "subnet_cidr_block" { default = "10.1.2.0/24" }
-variable "ami_value" { default = "ami-00dc79254d0461090" }
-variable "instance_type" { default = "t2.micro" }
-variable "subnet_cidr_block_2" { default = "10.1.3.0/24" }
-variable "no_of_public_subnet" { default = 2 }
-variable "no_of_private_subnet" { default = 2 }
-variable "in_subnets_max" { default = 4 }
